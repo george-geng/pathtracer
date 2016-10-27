fd95e49b417096ae8e2e1283b665f4f30af01978
George Geng <George@calvisitor-10-105-175-252.calvisitor.1918.berkeley.edu>
2016-10-27 15:02:36 -0700

    added build/cgl/dae files

---
+++ build/src/CMakeFiles/pathtracer.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/George/cs184/asst3_pathtracer/src/application.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/application.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/bbox.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/bbox.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/bsdf.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/bsdf.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/bvh.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/bvh.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/camera.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/camera.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/camera_info.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/camera_info.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/collada.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/collada.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/light_info.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/light_info.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/material_info.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/material_info.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/polymesh_info.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/polymesh_info.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/collada/sphere_info.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/collada/sphere_info.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/dynamic_scene/mesh.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/mesh.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/dynamic_scene/scene.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/scene.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/dynamic_scene/sphere.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/sphere.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/halfEdgeMesh.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/halfEdgeMesh.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/main.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/main.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/meshEdit.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/meshEdit.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/misc/sphere_drawing.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/misc/sphere_drawing.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/pathtracer.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/pathtracer.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/sampler.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/sampler.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/static_scene/environment_light.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/static_scene/environment_light.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/static_scene/light.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/static_scene/light.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/static_scene/object.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/static_scene/object.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/static_scene/sphere.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/static_scene/sphere.cpp.o"
+  "/Users/George/cs184/asst3_pathtracer/src/static_scene/triangle.cpp" "/Users/George/cs184/asst3_pathtracer/build/src/CMakeFiles/pathtracer.dir/static_scene/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "/usr/local/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/George/cs184/asst3_pathtracer/build/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/Users/George/cs184/asst3_pathtracer/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/Users/George/cs184/asst3_pathtracer/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
